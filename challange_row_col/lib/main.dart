da53c7384064629f60906649d6288dc837f2c250
Abhijit2505 <abhijittripathy99@gmail.com>
2020-06-19 12:11:36 +0530

    Added portfolio app Version 1

--- challange_row_col/lib/main.dart
+++ challange_row_col/lib/main.dart
@@ -10,42 +10,84 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Row(
-            ///mainAxisSize: MainAxisSize.min,
-            //crossAxisAlignment: CrossAxisAlignment.stretch,
+        body: Container(
+          padding: EdgeInsets.fromLTRB(0, 170, 0, 0),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3, 1],
+                  colors: [Colors.red, Colors.orange])),
+          child: Column(
             children: <Widget>[
-              Container(
-                width: 80.0,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 60.0,
+                backgroundImage: AssetImage('images/abhijit2.jpg'),
+              ),
+              Text(
+                "Abhijit Tripathy",
+                style: TextStyle(
+                  fontFamily: 'CaveatBrush',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                ),
               ),
               SizedBox(
-                width: 30.0,
+                height: 15,
               ),
-              Column(
-                children: <Widget>[
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    margin: EdgeInsets.fromLTRB(35.0, 240.0, 35.0, 0.0),
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    margin: EdgeInsets.fromLTRB(35.0, 0.0, 35.0, 0.0),
-                    color: Colors.green,
-                  ),
-                ],
+              Text(
+                "MACHINE LEARNING ENGINEER",
+                style: TextStyle(
+                  fontFamily: 'Comfortaa',
+                  fontSize: 15,
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                ),
               ),
               SizedBox(
-                width: 30.0,
+                height: 15,
               ),
               Container(
-                width: 80.0,
-                color: Colors.blue,
+                padding: EdgeInsets.fromLTRB(70, 10, 20, 10),
+                color: Colors.white,
+                margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+91 7008187611',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
               ),
+              Container(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  color: Colors.white,
+                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        "abhijittripathy99@gmail.com",
+                        style: TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
+                    ],
+                  ))
             ],
           ),
         ),
