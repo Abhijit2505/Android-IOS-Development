a4904c5a77a30aa9649133379eb9fa27433f1f10
Abhijit2505 <abhijittripathy99@gmail.com>
2020-07-25 15:39:38 +0530

    Added the database application

---
+++ fantastic_database/android/app/src/main/java/com/example/fantastic_database/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.fantastic_database;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ fantastic_database/lib/homepresenter.dart
@@ -0,0 +1,27 @@
+import 'package:fantastic_database/database/database_helper.dart';
+import 'package:fantastic_database/database/model/user.dart';
+import 'dart:async';
+
+abstract class HomeContract {
+  void screenUpdate();
+}
+
+class HomePresenter {
+  HomeContract _view;
+  var db = new DatabaseHelper();
+  HomePresenter(this._view);
+  delete(User user) {
+    var db = new DatabaseHelper();
+    db.deleteUsers(user);
+    updateScreen();
+  }
+
+  Future<List<User>> getUser() {
+    return db.getUser();
+  }
+
+  updateScreen() {
+    _view.screenUpdate();
+
+  }
+}
